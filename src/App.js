3a366ef5b501339d0b23cccc2583f2cdd9a46239
sudharsan004 <sudharsansriram8921@gmail.com>
2020-11-10 13:15:52 +0530

    added row

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import requests from './requests'
 function App() {
   return (
     <>
-    <h1>hello f2498ad04781ea57eef51b9c47ed7871</h1>
-    <Row title="Netflix Original" fetchUrl={requests.fetchNetflixOriginals}/>
-    <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+    <h1>hello </h1>
+    <Row title="Netflix Original" fetchURL={requests.fetchNetflixOriginals}/>
+    <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
     </>
-  );
+  )
 }
 
 export default App;
--- src/Row.js
+++ src/Row.js
@@ -3,15 +3,13 @@ import instance from './axios'
 function Row({ title, fetchURL }) {
     const [movies, setmovies] = useState([])
 
-    
-    
-
     return (
         <div>
             <h1>{title}</h1>
             {/* <h1>{response}</h1> */}
         </div>
     )
+
 }
 
 export default Row
