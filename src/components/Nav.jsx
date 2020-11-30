733618c20c6251df1202e8e009294d52c0c86e06
sudharsan004 <sudharsansriram8921@gmail.com>
2020-11-30 16:11:23 +0530

    update banner

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Nav.css'
 function Nav() {
+    const [showBlackNav, setshowBlackNav] = useState(false)
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                setshowBlackNav(true)
+            }
+            else { setshowBlackNav(false) }
+        })
+        return () => {
+            window.removeEventListener('scroll', this)
+        }
+    }, [])
     return (
-        <nav className="nav">
-            {/* <img className="nav__logo"
-                // src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+        <nav className="nav" style={showBlackNav ? { "backgroundColor": "#111" } : null}>
+            <img className="nav__logo"
+                src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo.png"
                 alt="Netflix-logo"
-            /> */}
-        </nav>
+            />
+            <img className="nav__avatar"
+                src=" https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+                alt="Netflix-logo"
+            />
+        </nav >
     )
 }
 
